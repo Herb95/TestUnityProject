be638d52f7dc507d763a9cea945179e57e445661
GraywolfZ <1406912774@qq.com>
2019-02-11 14:57:50 +0800

    提交项目

---
+++ InfinityScrollP/Assets/CloneList/ItemData.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
+
+namespace Assets.Test
+{
+    [Serializable]
+    public class ItemData
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ItemData()
+        {
+
+        }
+
+        public ItemData(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+    }
+
+    public static class GameUtils
+    {
+        /// <summary>
+        /// 利用System.Xml.Serialization来实现序列化与反序列化
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="realObject"></param>
+        /// <returns></returns>
+        public static T Clone<T>(T realObject)
+        {
+            using (Stream stream = new MemoryStream())
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(T));
+                serializer.Serialize(stream, realObject);
+                stream.Seek(0, SeekOrigin.Begin);
+                return (T) serializer.Deserialize(stream);
+            }
+        }
+
+        /// <summary>
+        /// 利用 System.Runtime.Serialization序列化与反序列化完成引用对象的复制
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="realObject"></param>
+        /// <returns></returns>
+        public static T Clone2<T>(T realObject)
+        {
+            using (Stream objectStream = new MemoryStream())
+            {
+                IFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(objectStream, realObject);
+                objectStream.Seek(0, SeekOrigin.Begin);
+                return (T) formatter.Deserialize(objectStream);
+            }
+        }
+    }
+}
---
+++ InfinityScrollP/Assets/PngToSprite/Editor/PngAutoSprite.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+public class PngAutoSprite : AssetPostprocessor
+{
+    //void OnPreprocessTexture()
+    //{
+    //    /*
+    //    //自动设置类型; 
+    //    string dirName =Path.GetDirectoryName(assetPath);
+    //    Debug.Log("Import --- " + dirName);
+    //    string fullPath = "Assets/Icon/icon" + "/";
+    //    if (dirName == fullPath)
+    //    {
+
+    //        TextureImporter textureImporter = (TextureImporter)assetImporter;
+    //        textureImporter.textureType = TextureImporterType.Sprite;
+
+    //        /*
+    //        string folderStr = Path.GetFileName(dirName);
+    //        Debug.Log("Set Packing Tag --- " + folderStr);
+    //        textureImporter.spritePackingTag = folderStr;
+    //        */
+    //    //}
+    //}
+    /*
+    private void OnPreprocessTexture()
+    {
+        if (assetPath.ToLower().IndexOf("Assets/Icon/", StringComparison.InvariantCultureIgnoreCase) != -1)
+        {
+            TextureImporter textureImporter = (TextureImporter)assetImporter;
+            textureImporter.textureType = TextureImporterType.Sprite;
+            textureImporter.spriteImportMode = SpriteImportMode.Single;
+            textureImporter.alphaIsTransparency = true;
+            textureImporter.mipmapEnabled = false;
+        }
+    }
+    */
+}
+
+
+public class PngToSpriteTools : AssetPostprocessor
+{
+
+    [MenuItem("Assets/图片处理/Png转Sprite")]
+    public static void PngToSprite()
+    {
+        UnityEngine.Object[] arr = Selection.GetFiltered(typeof(UnityEngine.Object), SelectionMode.TopLevel);
+        string path = Application.dataPath.Substring(0, Application.dataPath.LastIndexOf('/')) + "/" + AssetDatabase.GetAssetPath(arr[0]);
+        Debug.Log(path);
+        // OnPreprocessTexture();
+    }
+
+    public void OnPreprocessTexture()
+    {
+        TextureImporter textureImporter = (TextureImporter)assetImporter;
+        textureImporter.textureType = TextureImporterType.Sprite;
+        //textureImporter.spriteImportMode = SpriteImportMode.Single;
+        textureImporter.alphaIsTransparency = true;
+        textureImporter.mipmapEnabled = false;
+    }
+
+    /// <summary>
+    /// 循环设置选择的贴图
+    /// </summary>
+    private void LoopSetTexture()
+    {
+        System.Object[] textures = GetSelectedTextures();
+        foreach (Texture2D texture in textures)
+        {
+            string path = AssetDatabase.GetAssetPath(texture);
+            TextureImporter texImporter = GetTextureSettings(path);
+            TextureImporterSettings tis = new TextureImporterSettings();
+            texImporter.ReadTextureSettings(tis);
+            texImporter.SetTextureSettings(tis);
+            AssetDatabase.ImportAsset(path);
+        }
+    }
+
+    /// <summary>
+    /// 获取选择的贴图
+    /// </summary>
+    /// <returns></returns>
+    private UnityEngine.Object[] GetSelectedTextures()
+    {
+        return Selection.GetFiltered(typeof(Texture2D), SelectionMode.DeepAssets);
+    }
+
+    public TextureImporter GetTextureSettings(string path)
+    {
+        TextureImporter textureImporter = AssetImporter.GetAtPath(path) as TextureImporter;
+        textureImporter.anisoLevel = 1;
+        return textureImporter;
+    }
+}
